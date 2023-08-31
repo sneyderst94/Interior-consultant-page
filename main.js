8135876922a1ac567e2e55e693aae3ee143042b7
Sneyder Silva <sneyderst94@gmail.com>
2023-08-31 19:56:58 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const visible = document.querySelector(".visible");
+const menuMobile = document.querySelector(".menu__mobile");
+
+menuMobile.addEventListener("click", () => {
+  visible.classList.toggle("visible");
+});
