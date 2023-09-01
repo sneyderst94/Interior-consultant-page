38fff37037e73a8285de134688e815eaba8ca94f
Sneyder Silva <sneyderst94@gmail.com>
2023-09-01 07:22:58 -0400

    Add class 'close_menu' and funcionality

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const visible = document.querySelector(".visible");
-const menuMobile = document.querySelector(".menu__mobile");
+const menuMobile = document.querySelector(".menu_mobile");
+const closeMenu = document.querySelector(".close_menu");
 
 menuMobile.addEventListener("click", () => {
+  visible.classList.toggle("visible");
+});
+
+closeMenu.addEventListener("click", () => {
   visible.classList.toggle("visible");
 });
